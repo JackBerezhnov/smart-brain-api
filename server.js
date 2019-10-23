7117f824c79701df3ec1a8579de15a77257497c8
Evgeny Berezhnov <jackberezhnov@gmail.com>
2019-10-23 15:42:13 -0700

    create git

---
+++ server.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ],
+    login: [
+        {
+            id: '987',
+            has: '',
+            email: 'john@gmail.com'
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+        // Load hash from your password DB.
+    bcrypt.compare("apples", "$2a$10$0OyB9NUdZJRCi/baeGURNuH8gcnyehzj1hNV3WU44Jn6ECAqpO8gq", function(err, res) {
+        console.log('first guess', res);
+    });
+    bcrypt.compare("veggies", "$2a$10$0OyB9NUdZJRCi/baeGURNuH8gcnyehzj1hNV3WU44Jn6ECAqpO8gq", function(err, res) {
+        console.log('second guess', res);
+    });
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+        res.json(database.users[0]);
+    } else {
+        res.status(400).json('error logging in');
+    }
+})
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+           return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++
+           return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+});
